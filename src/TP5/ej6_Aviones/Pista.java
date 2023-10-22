e5a108107b089b4d45d94f8a1e89d562a6c9e7e4
giulivicentino <145300766+giulivicentino@users.noreply.github.com>
2023-10-22 13:37:42 -0300

    despeguemooo

---
+++ src/TP5/ej6_Aviones/Pista.java
@@ -0,0 +1,11 @@
+
+package TP5.ej6_Aviones;
+//RECURSO COMPARTIDO
+
+import java.util.concurrent.Semaphore;
+
+public class Pista {
+    private Semaphore mutexPista = new Semaphore(1); 
+    
+    
+}
